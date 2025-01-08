ce1616ba4e46713215c6721b675c7b407d6e8c24
Olexa-Klyus <olexakljus@gmail.com>
2025-01-08 20:24:56 +0200

    celery

--- backend/core/services/email_service.py
+++ backend/core/services/email_service.py
@@ -4,18 +4,23 @@ from django.contrib.auth import get_user_model
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import get_template
 
-from core.services.jwt_service import ActionToken, ActivateToken, JWTService, RecoveryToken
+from configs.celery import app
+
+from core.services.jwt_service import ActivateToken, JWTService, RecoveryToken
+
+UserModel = get_user_model()
 
 
 class EmailService:
-    @classmethod
-    def __send_email(cls, to: str, template_name: str, context: dict, subject: str) -> None:
+    @staticmethod
+    @app.task
+    def __send_email(to: str, template_name: str, context: dict, subject: str) -> None:
         template = get_template(template_name)
         html_content = template.render(context)
         msg = EmailMultiAlternatives(
             to=[to],
-            subject=subject,
-            from_email=os.environ.get('EMAIL_HOST_USER')
+            from_email=os.environ.get('EMAIL_HOST_USER'),
+            subject=subject
         )
         msg.attach_alternative(html_content, "text/html")
         msg.send()
@@ -24,7 +29,7 @@ class EmailService:
     def register(cls, user):
         token = JWTService.create_token(user, ActivateToken)
         url = f'http://localhost/activate/{token}'
-        cls.__send_email(
+        cls.__send_email.delay(
             to=user.email,
             template_name='register.html',
             context={'name': user.profile.name, 'url': url},
@@ -35,9 +40,20 @@ class EmailService:
     def recovery(cls, user):
         token = JWTService.create_token(user, RecoveryToken)
         url = f'http://localhost/auth/recovery/{token}'
-        cls.__send_email(
+        cls.__send_email.delay(
             to=user.email,
             template_name='recovery.html',
             context={'url': url},
             subject="Recovery"
         )
+
+    @staticmethod
+    # @app.task
+    def spam():
+        for user in UserModel.objects.all():
+            EmailService.__send_email(
+                to=user.email,
+                template_name='spam.html',
+                context={},
+                subject='SPAM'
+            )
